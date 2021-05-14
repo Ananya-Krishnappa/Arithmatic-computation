eb86bcd6fadd90e2164aa9e4cec05c5ca0054c8b
Ananya-Krishnappa <ananyak3395@gmail.com>
2021-05-14 20:28:01 +0530

    Added:fifth computation

--- arithmaticComputation.sh
+++ arithmaticComputation.sh
@@ -1,11 +1,16 @@
 #!/bin/bash -x
+declare -A resultsOfArith
 echo "enter the three inputs"
 read a b c
 result=$(($a+$b*$c))
 result1=$(($a*$b+$c))
 result2=$(($c+$a/$b))
-result3=$(($a%$b+$c))
+result3=$((a%b+c))
 echo $result
 echo $result1
 echo $result2
 echo $result3
+resultOfArith[result]=$result
+resultOfArith[result1]=$result1
+resultOfArith[result2]=$result2
+resultOfArith[result3]=$result3
